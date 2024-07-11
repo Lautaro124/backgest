856f879f37b8bd93814cafe9b13e9bf39748964c
Lautaro124 <lautaro.gonzalez@konecta-group.com>
2024-07-10 22:44:15 -0300

    Change account schema

--- src/common/schema/account.schema.ts
+++ src/common/schema/account.schema.ts
@@ -4,24 +4,33 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 export class Account {
   @Prop({
     required: true,
+    type: String,
   })
   name: string;
 
   @Prop({
     required: true,
     default: 0,
+    type: Number,
   })
   value: number;
 
   @Prop({
     required: true,
+    type: String,
   })
   category: string;
 
   @Prop({
     required: true,
+    type: String,
   })
   dni: string;
+  @Prop({
+    required: true,
+    type: String,
+  })
+  exchange: string;
 }
 
 export const AccountSchema = SchemaFactory.createForClass(Account);
